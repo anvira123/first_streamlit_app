60ebb9123b26d370dba697cde92180ac56cf1dea
anvira123 <148154198+anvira123@users.noreply.github.com>
2023-10-16 23:13:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Dinner')
 
-streamlit.header('Breakfast')
-streamlit.text('1')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
