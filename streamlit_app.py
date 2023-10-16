2b7594e1aa9cf9422440084e5dd8aee225c2ea6e
anvira123 <148154198+anvira123@users.noreply.github.com>
2023-10-16 23:15:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas
 
-streamlit.title('My Parent New Healthy Dinner')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.dataframe(my_fruit_list)
+
+# streamlit.title('My Parent New Healthy Dinner')
+
+# streamlit.header('Breakfast Menu')
+# streamlit.text('Omega 3 & Blueberry Oatmeal')
+# streamlit.text('Kale, Spinach & Rocket Smoothie')
+# streamlit.text('Hard-Boiled Free-Range Egg')
